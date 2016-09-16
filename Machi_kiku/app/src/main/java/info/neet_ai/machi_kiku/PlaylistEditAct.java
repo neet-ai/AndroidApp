a6a2a76d08c18dbd0c13d027c9b2ae2aeb04b6f3
kato-hiroto <kato-hiroto@ed.tmu.ac.jp>
2016-09-16 20:56:13 +0900

    特に進捗なし
    zipはプロジェクトをまとめたもの（バックアップ）

--- Machi_kiku/app/src/main/java/info/neet_ai/machi_kiku/PlaylistEditAct.java
+++ Machi_kiku/app/src/main/java/info/neet_ai/machi_kiku/PlaylistEditAct.java
@@ -3,11 +3,11 @@ package info.neet_ai.machi_kiku;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class PlaylistEditAct extends AppCompatActivity {
+public class PlaylistEditAct extends CommonAct{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.playlist_edit_layout);
+        super.onCreate(savedInstanceState);
     }
 }
